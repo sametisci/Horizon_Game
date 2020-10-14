f53cd83c0bee16e785b6cd277f40d37a92138a9e
CanKRVN <erdacan54@gmail.com>
2020-10-14 16:15:07 +0300

    kod eklemeler
    hareket kodu, puan göstergesi, touchpad

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    public float speed, forwardSpeed, tilt, distance;
+    private int money, score;
+    public static int highScore;
+    private Quaternion calibrationQuaternion;
+    public SimpleTouchPad touchPad;
+    public Text distanceText, gameOverText,highScoreText,currentScore;
+    public GameObject GameOverPanel;
+    public GameObject ship;
+    private bool isGameOver;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        CalibrateAccelerometer();
+
+        PlayerPrefs.GetInt("HighScore");
+    }
+
+    private void Update()
+    {
+        distance = transform.position.z * 0.2f;
+        score = Convert.ToInt32(distance);
+        distanceText.text = score.ToString();
+
+        highScoreText.text = "BEST: " + highScore;
+        currentScore.text = "Current Score: " + score;
+    }
+
+
+    void CalibrateAccelerometer()
+    {
+        Vector3 accelerationSnapshot = Input.acceleration;
+        Quaternion rotateQuaternion = Quaternion.FromToRotation(new Vector3(0.0f, 0.0f, -1.0f), accelerationSnapshot);
+        calibrationQuaternion = Quaternion.Inverse(rotateQuaternion);
+    }
+
+    //Get the 'calibrated' value from the Input
+    Vector3 FixAcceleration(Vector3 acceleration)
+    {
+        Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+        return fixedAcceleration;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = touchPad.GetDirection();
+
+        Vector3 movement = new Vector3(direction.x, 0.0f, forwardSpeed);
+
+        if (SimpleTouchPad.isTouchStat || isGameOver)
+        {
+            rb.velocity = Vector3.zero;
+        }
+        else
+        {
+            rb.velocity = movement * speed;
+        }
+    }
+
+   
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(ship);
+            
+
+            GameOverPanel.SetActive(true);
+            isGameOver = true;
+            
+            if(score > highScore)
+            {
+                Debug.Log("HİGH SCORE!");
+                highScore = score;
+                
+                PlayerPrefs.SetInt("HighScore", highScore);
+                PlayerPrefs.Save();
+            }
+        }
+    }
+
+}
